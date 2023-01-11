93ae419cf45150a3d9c1ea1e0eeabcf8026b1e09
Yasmin <yasmin.5126c@gmail.com>
2023-01-11 15:54:38 -0300

    tela principal

---
+++ src/TelaBase.java
@@ -0,0 +1,20 @@
+package src;
+
+import javax.swing.*;
+import java.io.Serial;
+
+public class TelaBase extends JFrame{
+
+        @Serial
+        private static final long serialVersionUID = 1L;
+        public TelaBase() {
+            setTitle("Snack Game");
+            setSize(600,400); // tamanho da tela
+            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+            setLocationRelativeTo(null);
+            setLayout(null);
+            setResizable(false); //altera tamanho da tela
+            setIconImage(new ImageIcon("res/img/icon.png").getImage());
+        }
+    }
+}
